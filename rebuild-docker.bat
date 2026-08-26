f659a273fd21691ff396d64c52683dada044781e
Your Name <your.email@example.com>
2026-08-26 11:56:26 +0530

    chore(build): add docker hub push to rebuild script

--- rebuild-docker.bat
+++ rebuild-docker.bat
@@ -2,3 +2,6 @@
 REM After code changes: rebuild images, recreate containers, prune old images.
 call "%~dp0config\startup\rebuild_docker.bat" %*
 exit /b %ERRORLEVEL%
+@echo off
+call "%~dp0config\startup\push_docker_hub.bat" %*
+exit /b %ERRORLEVEL%
